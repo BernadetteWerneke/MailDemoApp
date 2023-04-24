d86965cb61c6171eca7873c8b7c7f4f62dd52b98
Bernadette <120725476+BernadetteWerneke@users.noreply.github.com>
2023-04-24 15:08:17 +0200

    Initial Commit

---
+++ MailDemoApp/MailDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailDemoAppApp.swift
+//  MailDemoApp
+//
+//  Created by Bernadette Werneke on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MailDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
